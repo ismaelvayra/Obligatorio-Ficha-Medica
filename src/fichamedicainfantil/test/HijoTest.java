18453abdb1cb02e2d56999aa6003d18b72e788d8
Ismael Vayra <ismael.vayra@gmail.com>
2015-11-05 00:25:04 -0300

    Agrega las clases de test vacias

---
+++ src/fichamedicainfantil/test/HijoTest.java
@@ -0,0 +1,109 @@
+package fichamedicainfantil.test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tano on 05/11/15.
+ */
+public class HijoTest {
+
+    @org.junit.Test
+    public void testGetNombre() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetNombre() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetApellido() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetApellido() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetPeso() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetPeso() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetTalla() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetTalla() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetPerimetroEncefalico() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetPerimetroEncefalico() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetGrupoSanguino() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetGrupoSanguino() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetAnomalias() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetAnomalias() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetPatologia() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetPatologia() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetListaConsultas() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetListaConsultas() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetListaVacunas() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetListaVacunas() throws Exception {
+
+    }
+}
